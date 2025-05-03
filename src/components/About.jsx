be3885bb910786b9fa2e4ea059bfc6b2c6f17563
Parthipan Kavin <kavin@Kavins-MacBook-Pro.local>
2025-05-04 00:39:36 +0800

    edited about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,45 +10,12 @@ const About = () => {
                     <img src={AboutImg} alt=""
                         className='w-72 h-80 rounded object-cover mb-8 md:mb-0'/>
                     <div className='flex-1'>
-                        <p className='mt-4 text-lg py-6'>
-                            I'm a Business Analytics undergraduate at National University of Singapore(NUS) with a passion for turning data into insights and building cool tech solutions. I love tackling challenges and finding smart, efficient ways to solve problems. I'm a charismatic and driven individual—always eager to learn, take the lead when needed and put in the hard work to make things happen. Whether it’s coding, analyzing trends, or brainstorming innovative ideas, I thrive on making an impact and continuously growing along the way.
+                        <p className='mt-4 text-lg py-2'>
+                            I'm a Business Analytics undergraduate at the National University of Singapore (NUS) with a strong interest in software engineering and data-driven solutions. I enjoy designing and building scalable, user-focused applications that solve real-world problems. From writing clean, efficient code to deploying full-stack projects, I’m always eager to push my technical boundaries and explore new tools and frameworks.
                         </p>
-                        <div className='flex items-center py-1'>
-                            <label htmlFor="htmlandcss" className='w-2/12'>Full-stack Development</label>
-                            <div className='grow bg-gray-800 rounded-full h-2.5'>
-                                <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full
-                                                transform transition-transform duration-300 hover:scale-105 w-11/12'>
-                                                    
-                                </div>
-                            </div>
-                        </div>
-                        <div className='flex items-center py-1'>
-                            <label htmlFor="htmlandcss" className='w-2/12'>Data Analysis</label>
-                            <div className='grow bg-gray-800 rounded-full h-2.5'>
-                                <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full
-                                                transform transition-transform duration-300 hover:scale-105 w-10/12'>
-
-                                </div>
-                            </div>
-                        </div>
-                        <div className='flex items-center py-1'>
-                            <label htmlFor="htmlandcss" className='w-2/12'>Management</label>
-                            <div className='grow bg-gray-800 rounded-full h-2.5'>
-                                <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full
-                                                transform transition-transform duration-300 hover:scale-105 w-11/12'>
-
-                                </div>
-                            </div>
-                        </div>
-                        <div className='flex items-center py-1'>
-                            <label htmlFor="htmlandcss" className='w-2/12'>Machine Learning</label>
-                            <div className='grow bg-gray-800 rounded-full h-2.5'>
-                                <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full
-                                                transform transition-transform duration-300 hover:scale-105 w-8/12'>
-
-                                </div>
-                            </div>
-                        </div>   
+                        <p className='mt-4 text-lg py-2'>
+                            I bring a mix of analytical thinking, creativity, and leadership. Whether it’s contributing to collaborative codebases, optimizing performance, or leading innovative tech initiatives, I thrive in fast-paced, problem-solving environments. I'm driven by impact, continuously learning, and excited to take on complex challenges that create real value.
+                        </p>   
                     </div>
                 </div>
             </div>
